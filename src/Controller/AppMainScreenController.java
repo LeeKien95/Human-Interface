7c41c0ee8228b49ce25ea0a116c64ac0a6cd00c8
KienLeTrung <kienletrung.1995@gmail.com>
2017-04-20 13:09:26 +0700

    setting

---
+++ src/Controller/AppMainScreenController.java
@@ -0,0 +1,93 @@
+package Controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class AppMainScreenController extends Application implements Initializable{
+	@FXML ImageView doorButton;
+	
+	public static Stage primaryStage;
+	public static BorderPane mainLayout;
+	
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void btnDoorClickedNotify() {
+		System.out.println("Door!");
+	}
+	
+	public void fingerPrintSettingAction() {
+		System.out.println("Finger Print is not available right now.");
+	}
+	
+	public void seatPositionsSettingAction() {
+		System.out.println("Seat Positions is not available right now.");
+	}
+	
+	public void voiceSettingAction() {
+		System.out.println("Voice Setting is not available right now.");
+	}
+	
+	public void cameraSettingAction() {
+		System.out.println("Camera setting is not available right now.");
+	}
+	
+	public void userSettingAction() {
+		System.out.println("User setting is not available right now.");
+	}
+
+
+	public void showAppScreen() throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("/Screen/AppMainScreen.fxml"));
+		mainLayout = loader.load();
+		Scene scene = new Scene(mainLayout);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
+	public void showSettingScreen() throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("/Screen/AppSettingScreen.fxml"));
+		mainLayout = loader.load();
+		Scene scene = new Scene(mainLayout);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
+	public void showVoiceScreen() throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("/Screen/VoiceScreen.fxml"));
+		mainLayout = loader.load();
+		Scene scene = new Scene(mainLayout);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
+
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		this.primaryStage = primaryStage;
+//		this.primaryStage.setTitle("AppScreen");
+		showAppScreen();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
