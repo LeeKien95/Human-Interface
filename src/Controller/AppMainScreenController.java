341670156d819b45d6f35ca0dcffb4a70a099001
LeeKien95 <kienletrung.1995@gmail.com>
2017-04-20 14:49:22 +0700

    active stuff

--- src/Controller/AppMainScreenController.java
+++ src/Controller/AppMainScreenController.java
@@ -1,8 +1,11 @@
 package Controller;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javax.imageio.ImageIO;
 
 import javafx.application.Application;
 import javafx.fxml.FXML;
@@ -13,9 +16,16 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import sun.applet.Main;
 
 public class AppMainScreenController extends Application implements Initializable{
 	@FXML ImageView doorButton;
+	@FXML ImageView antiThiefButton;
+	@FXML ImageView packingButton;
+	
+	private boolean activeDoor = false;
+	private boolean activeAntiThief = false;
+	private boolean activePacking = false;
 	
 	public static Stage primaryStage;
 	public static BorderPane mainLayout;
@@ -48,7 +58,46 @@ public class AppMainScreenController extends Application implements Initializabl
 	public void userSettingAction() {
 		System.out.println("User setting is not available right now.");
 	}
-
+	
+	public void activeFingerPrint() {
+		System.out.println("Active Finger Print");
+		Image img;
+		if(activeDoor) {
+			img = new Image("file:images/icons/icon_hand-pointer-o.png");	
+			activeDoor = false;
+		} else {
+			img = new Image("file:images/green_hand.png");
+			activeDoor = true;
+		}
+        doorButton.setImage(img);
+	}
+	
+	public void activeAntiThief() {
+		System.out.println("Active Anti Thief");
+		Image img;
+		if(activeAntiThief) {
+			img = new Image("file:images/icons/icon_user-times.png");	
+			activeAntiThief = false;
+		} else {
+			img = new Image("file:images/green_anti_thief.png");
+			activeAntiThief = true;
+		}
+        antiThiefButton.setImage(img);
+	}
+	
+	public void activePacking() {
+		System.out.println("Active Packing");
+		Image img;
+		if(activePacking) {
+			img = new Image("file:images/icons/icon_automobile.png");	
+			activePacking = false;
+		} else {
+			img = new Image("file:images/green_car.png");
+			activePacking = true;
+		}
+        packingButton.setImage(img);
+	}
+	
 
 	public void showAppScreen() throws IOException {
 		FXMLLoader loader = new FXMLLoader();
